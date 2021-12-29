be3192258fcfa6d304c27f07c65bf7954a3f6eac
mudit0299 <muditgupta.rocks@gmail.com>
2021-12-29 12:17:11 +0530

    Internationalization

--- restful-web-services/src/main/java/com/example/demo/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/example/demo/helloworld/HelloWorldController.java
@@ -1,13 +1,23 @@
 package com.example.demo.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	@Autowired
+	private MessageSource messageSource;
+	
+	
 	@GetMapping("/hello-world")
 	//@RequestMapping(method =RequestMethod.GET,path = "/hello-world")
 	public String helloWorld() {
@@ -25,5 +35,12 @@ public class HelloWorldController {
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello World, %s", name));
 		
+	}
+	@GetMapping("/hello-world-internationalized")
+	public String helloWorldInternationalized(
+			//@RequestHeader(name="Accept-Language",required = false) Locale locale
+			) {
+		return messageSource
+				.getMessage("good.morning.message",null,"Default Message", LocaleContextHolder.getLocale());
 	}
 }
